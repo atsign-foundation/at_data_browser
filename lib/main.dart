4c4e9f068611ff8bb4ce25e60c903590e167a584
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-05-17 12:47:48 -0400

    fix: added missing widgets due to merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'dart:async';
 
 import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
 import 'package:at_client_mobile/at_client_mobile.dart';
-<<<<<<< HEAD
 import 'package:at_contacts_flutter/utils/init_contacts_service.dart';
 import 'package:at_data_browser/screens/apps_screen.dart';
 import 'package:at_data_browser/screens/browse_screen.dart';
 import 'package:at_data_browser/screens/connected_atsigns_screen.dart';
 import 'package:at_data_browser/screens/data_storage_screen.dart';
+import 'package:at_data_browser/screens/home_screen.dart';
 import 'package:at_data_browser/screens/settings_screen.dart';
 import 'package:at_data_browser/utils/theme.dart';
 import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
@@ -16,15 +16,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' show getApplicationSupportDirectory;
-=======
-import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
-import 'package:at_utils/at_logger.dart' show AtSignLogger;
-import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart'
-    show getApplicationSupportDirectory;
->>>>>>> upstream/trunk
 
-import 'home_screen.dart';
+import './data/navigation_service.dart';
 
 final AtSignLogger _logger = AtSignLogger(AtEnv.appNamespace);
 
@@ -36,7 +29,7 @@ Future<void> main() async {
   } catch (e) {
     _logger.finer('Environment failed to load from .env: ', e);
   }
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 Future<AtClientPreference> loadAtClientPreference() async {
@@ -67,7 +60,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       navigatorKey: NavigationService.navKey,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
@@ -118,49 +110,11 @@ class _MyAppState extends State<MyApp> {
         BrowseScreen.route: (_) => const BrowseScreen(),
         SettingsScreen.route: (_) => const SettingsScreen(),
       },
-=======
-      // * The onboarding screen (first screen)
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('MyApp'),
-        ),
-        body: Builder(
-          builder: (context) => Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                AtOnboardingResult onboardingResult =
-                    await AtOnboarding.onboard(
-                  context: context,
-                  config: AtOnboardingConfig(
-                    atClientPreference: await futurePreference,
-                    rootEnvironment: AtEnv.rootEnvironment,
-                    domain: AtEnv.rootDomain,
-                    appAPIKey: AtEnv.appApiKey,
-                  ),
-                );
-                switch (onboardingResult.status) {
-                  case AtOnboardingResultStatus.success:
-                    _goLocalData(context);
-                    break;
-                  case AtOnboardingResultStatus.error:
-                    _handleError(context);
-                    break;
-                  case AtOnboardingResultStatus.cancel:
-                    break;
-                }
-              },
-              child: const Text('Onboard an @sign'),
-            ),
-          ),
-        ),
-      ),
->>>>>>> upstream/trunk
     );
   }
 
   void _goLocalData(context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+    Navigator.push(context, MaterialPageRoute(builder: (_) => const HomeScreen()));
   }
 
   void _handleError(context) {
